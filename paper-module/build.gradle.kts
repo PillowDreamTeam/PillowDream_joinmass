e470aeadc489444bb959b3f7da99de61c1cd22fb
BaiYing <baiyingmc@outlook.com>
2026-02-13 05:53:00 +0800

    移除子模块重复的plugins声明

--- paper-module/build.gradle.kts
+++ paper-module/build.gradle.kts
@@ -1,8 +1,3 @@
-plugins {
-    id("java")
-    id("com.github.johnrengelman.shadow")
-}
-
 dependencies {
     // Paper API
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
@@ -14,7 +9,7 @@ tasks.shadowJar {
     archiveBaseName.set("PillowDream_joinmass-Paper")
     archiveVersion.set("")
     relocate("com.mysql.cj", "com.baiying.pillowdream.libs.mysql")
-    destinationDirectory.set(file("../build/libs")) // 统一输出到根项目build/libs
+    destinationDirectory.set(file("../build/libs"))
 }
 
 tasks.build {
